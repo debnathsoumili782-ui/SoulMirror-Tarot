a42ca725ddb75d1cdfdae5de5c2ae786bbf50727
Soumili Debnath <debnathsoumili782@gmail.com>
2026-08-21 03:25:14 +0530

    Fix database configuration

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+
+os.makedirs(INSTANCE_DIR, exist_ok=True)
+
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
     SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(BASE_DIR, "instance", "soulmirror.db")
+        "sqlite:///" + os.path.join(INSTANCE_DIR, "soulmirror.db")
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -15,6 +19,14 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
 
-    MAIL_USERNAME = "soulmirrortarot1221@gmail.com"
-    MAIL_PASSWORD = "yvhw rhxg jbyj vpcp"
-    MAIL_DEFAULT_SENDER = "soulmirrortarot1221@gmail.com"
+    MAIL_USERNAME = os.getenv(
+        "MAIL_USERNAME",
+        "soulmirrortarot1221@gmail.com"
+    )
+
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+    MAIL_DEFAULT_SENDER = os.getenv(
+        "MAIL_DEFAULT_SENDER",
+        "soulmirrortarot1221@gmail.com"
+    )
